8e7b261e7a48af21ebaf5ab45e65abe8f1c276e1
Joaquin Andresen <joaquin_andresen@icloud.com>
2026-01-11 15:39:11 -0300

    Elimino metodos viejos de login

--- backend/serializers/user_serializer.py
+++ backend/serializers/user_serializer.py
@@ -2,14 +2,11 @@ import uuid
 
 from djoser.serializers import UserCreateSerializer, User, UserSerializer
 from rest_framework import serializers
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-from backend.api_exceptions import InvalidImageError, StudentNotApprovedYetError
+from backend.api_exceptions import InvalidImageError
 from backend.models import User
 from backend.services import AwsS3Service
-from backend.services.auth_fiuba_service import AuthFiubaService
 from backend.services.image_validator_service import ImageValidatorService
-from backend.services.siu_service import SiuService
 
 
 class UserCustomCreateSerializer(UserCreateSerializer):
@@ -53,60 +50,6 @@ class UserCustomGetSerializer(UserSerializer):
     class Meta:
         model = User
         fields = ('dni', 'email', 'first_name', 'last_name', 'is_student', 'is_teacher', 'file')
-
-
-class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
-    username_field = 'code'
-
-    class Meta:
-        model = User
-        fields = ('code',)
-
-    def validate(self, attrs):
-        token_response = self._fiuba_service().get_token(self.context['request'].data['code'], self.context['request'].data['redirect_uri'])
-
-        auth_user_info = self._fiuba_service().userinfo(token_response['access_token'])
-
-        user = User.objects.get(dni=auth_user_info['sub'])
-
-        if not user.first_name:
-            self._update_user(user, auth_user_info)
-
-        data = {}
-
-        refresh = self.get_token(user)
-
-        data['refresh'] = str(refresh)
-        data['access'] = str(refresh.access_token)
-        self._validated_data = data
-
-        return data
-
-    def is_valid(self, raise_exception=False):
-        return self.validate(self.initial_data)
-
-    def _fiuba_service(self):
-        return AuthFiubaService()
-
-    def _update_user(self, user, auth_user_info):
-        siu_user_info = self._get_siu_user(user.is_student, user.dni)
-        # siu_user_info = { 'file': '328123' }
-
-        user.first_name = auth_user_info['name']
-        user.last_name = auth_user_info['family_name']
-
-        if user.is_student:
-            if not user.student.inscripto:
-                raise StudentNotApprovedYetError()
-            user.student.padron = siu_user_info['file']
-            user.student.save()
-        else:
-            user.teacher.legajo = siu_user_info['file']
-            user.teacher.save()
-        user.save()
-
-    def _get_siu_user(self, is_student, dni):
-        return SiuService().get_student(dni) if is_student else SiuService().get_teacher(dni)
 
 
 class SimpleLoginSerializer(serializers.Serializer):
