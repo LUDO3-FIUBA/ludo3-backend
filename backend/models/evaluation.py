28d60d05f27e7ec2b72e193aaeb0f4681f61f6da
Gonzalo Gordyn Biello <ggordyn@fi.uba.ar>
2026-03-16 18:21:58 -0300

    QR y verificacion de identidad optativa en evaluaciones

--- backend/models/evaluation.py
+++ backend/models/evaluation.py
@@ -10,6 +10,8 @@ class Evaluation(models.Model):
     evaluation_name = models.CharField(max_length=100, db_index=True, editable=False, verbose_name="Nombre de Evaluacion")
     is_graded = models.BooleanField(default=True)
     passing_grade = models.IntegerField(db_index=True, null=True, blank=True, verbose_name="Nota de Aprobacion")
+    requires_qr = models.BooleanField(default=False, verbose_name="Requiere escanear QR")
+    requires_identity = models.BooleanField(default=False, verbose_name="Requiere verificación de identidad")
 
     parent_evaluation = models.OneToOneField('self', on_delete=models.CASCADE, null=True, blank=True, verbose_name="Evaluacion a recuperar", related_name='make_up_evaluation')
     """Apunta a evaluacion 'padre'. Si esta definido, entonces esta Evaluation es un recuperatorio."""
--- backend/models/evaluation_submission.py
+++ backend/models/evaluation_submission.py
@@ -11,8 +11,8 @@ from .teacher import Teacher
 class EvaluationSubmission(models.Model):
     evaluation = models.ForeignKey(Evaluation, on_delete=models.CASCADE, related_name='submissions', verbose_name="Evaluacion")
     student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='submissions', verbose_name="Estudiante")
-    grade = models.IntegerField(null=True, db_index=True, verbose_name="Nota")
-    grader = models.ForeignKey(Teacher, null=True, on_delete=models.CASCADE, related_name='grader', verbose_name="Grader")
+    grade = models.IntegerField(null=True, blank=True, db_index=True, verbose_name="Nota")
+    grader = models.ForeignKey(Teacher, null=True, blank=True, on_delete=models.CASCADE, related_name='grader', verbose_name="Grader")
     
     file = models.FileField(upload_to='evaluation_submissions/', null=True, blank=True)
     submission_url = models.URLField(null=True, blank=True)
@@ -21,9 +21,9 @@ class EvaluationSubmission(models.Model):
     created_at = models.DateTimeField(default=timezone.now, editable=False, verbose_name="Creado en")
     updated_at = models.DateTimeField(default=timezone.now, verbose_name="Última actualización")
 
-    def clean(self):
-        if not any([self.file, self.submission_url, self.submission_text]):
-            raise ValidationError("At least one submission format is required: file, URL, or text.")
+    # def clean(self):
+    #     if not any([self.file, self.submission_url, self.submission_text]):
+    #         raise ValidationError("At least one submission format is required: file, URL, or text.")
 
     def __str__(self):
         return f"{self.student} - {self.evaluation} - {self.grade}"
