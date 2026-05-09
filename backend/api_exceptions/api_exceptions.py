79ca930b8dc2d3cfd4965afb879dc0ab67fbc8a4
imasuarezpino <isuarezp@fi.uba.ar>
2026-05-09 20:48:28 -0300

    feat(exceptions): add attendance location validation exceptions

--- backend/api_exceptions/api_exceptions.py
+++ backend/api_exceptions/api_exceptions.py
@@ -41,3 +41,15 @@ class UserTypeMisMatch(APIException):
 class InvalidSubjectCodeError(APIException):
     status_code = status.HTTP_404_NOT_FOUND
     default_code = 'invalid_subject_code'
+
+
+class AttendanceAlreadyValidError(APIException):
+    status_code = status.HTTP_403_FORBIDDEN
+    default_code = 'attendance_already_valid'
+    default_detail = 'Ya diste tu presente y fuiste anotado en la planilla.'
+
+
+class LocationAttendanceWindowExpiredError(APIException):
+    status_code = status.HTTP_403_FORBIDDEN
+    default_code = 'location_attendance_window_expired'
+    default_detail = 'Ya pasaron los 10 minutos disponibles para marcar presencia por ubicación.'
