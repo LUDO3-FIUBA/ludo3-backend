e0e802fcfca68f733d9cff27752fdd6a7620a3ca
Valentina <vlanzillotta@fi.uba.ar>
2026-06-29 11:42:17 -0300

    actualizo fake siu con materias y horarios para la oferta

--- fake-siu/server.js
+++ fake-siu/server.js
@@ -6,6 +6,18 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(jsonServer.rewriter({
+  "/v2/comisiones/:id": "/comisiones/:id",
+  "/v2/comisiones": "/comisiones",
+  "/v2/materias/:id": "/materias/:id",
+  "/v2/materias": "/materias",
+  "/v2/alumnos/:id": "/alumnos/:id",
+  "/v2/alumnos": "/alumnos",
+  "/v2/docentes/:id": "/docentes/:id",
+  "/v2/docentes": "/docentes",
+  "/v2/finales/:id": "/finales/:id",
+  "/v2/finales": "/finales",
+  "/v2/info": "/noop",
+  "/v2/status": "/noop",
   "/docentes/:docente_id/finales/:final_id/subir_acta": "/docentes/:docente_id/actas",
   "/docentes/:docente_id/finales/:final_id/cargar_notas": "/noop",
 }))
